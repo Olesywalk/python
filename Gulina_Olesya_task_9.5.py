2873bc49372415810d58e1d791556665a1e0b5f0
Olesywalk <100895765+Olesywalk@users.noreply.github.com>
2022-04-21 02:14:11 +0300

    Add files via upload

---
+++ Gulina_Olesya_task_9.5.py
@@ -0,0 +1,38 @@
+# 9.5. Реализовать класс Stationery (канцелярская принадлежность).
+# определить в нём атрибут title (название) и метод draw (отрисовка). Метод выводит сообщение «Запуск отрисовки»;
+# создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер);
+# в каждом классе реализовать переопределение метода draw. Для каждого класса метод должен выводить
+# уникальное сообщение;
+# создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print('Р—Р°РїСѓСЃРє РѕС‚СЂРёСЃРѕРІРєРё')
+
+
+class Pen(Stationery):
+    def draw(self):
+        print('Р СѓС‡РєР° СЂРёСЃСѓРµС‚')
+
+
+class Pencil(Stationery):
+    def draw(self):
+        super(Pencil, self).draw()
+        print('РљР°СЂР°РЅРґР°С€ СЂРёСЃСѓРµС‚')
+
+
+class Handle(Stationery):
+    def draw(self):
+        print('РњР°СЂРєРµСЂ СЂРёСЃСѓРµС‚')
+
+
+pen = Pen('СЂСѓС‡РєР°')
+pencil = Pencil('РєР°СЂР°РЅРґР°С€')
+handle = Handle('РјР°СЂРєРµСЂ')
+
+pen.draw()
+pencil.draw()
+handle.draw()
